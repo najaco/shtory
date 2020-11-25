4c494c93dec690d4c4b3742f91b742d8c7307d84
najaco <ncohen4299@gmail.com>
2020-11-25 12:50:39 -0500

    Added script subprocess call

--- shtory.js
+++ shtory.js
@@ -1,7 +1,4 @@
 const { execSync, spawnSync } = require('child_process');
 const SCRIPT_PATH = "my_script" + Date.now()
-// const script = execSync('script', ['-q', SCRIPT_PATH]);
 const script = spawnSync('script', ['-q', SCRIPT_PATH], {stdio: 'inherit'});
 
-
-// e
